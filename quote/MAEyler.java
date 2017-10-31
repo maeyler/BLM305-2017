a3846d1304dfa4c33f16167ff00673cf45d41ab6
M Akif Eyler <ma.eyler@gmail.com>
2017-10-31 21:16:06 +0300

    CW5
    Thread

---
+++ quote/MAEyler.java
@@ -0,0 +1,16 @@
+package quote;
+
+public class MAEyler implements Quotation {
+    public int year() {
+        return 1938;
+    }
+    public String text() {
+        return "Try not to become a man of success, but rather try to become a man of value";
+    }
+    public String author() {
+        return "Albert Einstein";
+    }
+    public String reference() {
+        return "http://www.brainyquote.com/quotes/authors/a/albert_einstein.html";
+    }
+}
---
+++ quote/Quotation.java
@@ -0,0 +1,8 @@
+package quote;
+
+interface Quotation {
+    int year();
+    String text();
+    String author();
+    String reference();
+}
