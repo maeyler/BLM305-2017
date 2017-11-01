8392a493f68bb6f7596c79567d1ef3779dac6314
Small Simple Safe <small.simple@gmail.com>
2017-11-01 09:43:58 +0300

    Test

---
+++ quote/Test.java
@@ -0,0 +1,16 @@
+package quote;
+
+public class Test implements Quotation {
+    public int year() {
+        return 2017;
+    }
+    public String text() {
+        return "Uyanık olun";
+    }
+    public String author() {
+        return "Mert";
+    }
+    public String reference() {
+        return "https://www.wikiwand.com/en/Small_Is_Beautiful";
+    }
+}
