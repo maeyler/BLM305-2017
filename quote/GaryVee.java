9dc3c043020f62013da0a77f9d3f6cc76bc5635c
Enes Kamil YILMAZ <nskmlylm@gmail.com>
2017-11-01 11:53:32 +0300

    garyVee quote added

---
+++ quote/GaryVee.java
@@ -0,0 +1,16 @@
+package quote;
+
+public class GaryVee implements Quotation {
+    public int year() {
+        return 2015;
+    }
+    public String text() {
+        return "“We only get to play this game one time. We have one life.”";
+    }
+    public String author() {
+        return "Gary Vaynerchuk";
+    }
+    public String reference() {
+        return "https://www.goalcast.com/2016/06/21/20-inspiring-gary-vaynerchuk-quotes/";
+    }
+}
