cadcafddf7683ea4b65c8f8e469b97265e406691
Unknown <mertdonmez19077@gmail.com>
2017-11-02 17:09:26 +0300

    MertDonmez-J.J-Ronsein-quote

---
+++ quote/MertDonmez.java
@@ -0,0 +1,18 @@
+
+    package quote;
+
+public class MertDonmez implements Quotation {
+    public int year() {
+        return 2017;
+    }
+    public String text() {
+        return "Ýnsan düþünmek, inanmak daha da önemlisi sevmek için dünyaya gelmiþtir.";
+    }
+    public String author() {
+        return "J.J. Ronsein";
+    }
+    public String reference() {
+        return "https://www.frmtr.com/hayat-bilgisi/872744-dunyanin-en-guzel-ve-degerli-sozleri-bilim-adamlarindan-sayfa-1-17-a.html";
+    }
+ 
+}
