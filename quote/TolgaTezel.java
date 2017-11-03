7b547c3c4054d2160172202a65a9f59e3815764d
Unknown <ttezel46@gmail.com>
2017-11-03 14:12:48 +0300

    Tolga tezel quote

---
+++ quote/TolgaTezel.java
@@ -0,0 +1,16 @@
+package quote;
+
+public class TolgaTezel implements Quotation {
+    public int year() {
+        return 1925;
+    }
+    public String text() {
+        return "Authority, without any condition and reservation, belongs to the nation.  ";
+    }
+    public String author() {
+        return "Mustafa Kemal ATATURK";
+    }
+    public String reference() {
+        return "http://www.ingilizcechat.net/ataturk/ataturkun_sozleri/";
+    }
+}
