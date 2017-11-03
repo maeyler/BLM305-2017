6746079a3d2941521a6bce3881fe7a664eaae8b9
omeryazici <yaziciomer94@gmail.com>
2017-11-03 21:15:23 +0300

    by Ömer Yazıcı

---
+++ quote/OmerYazici.java
@@ -0,0 +1,16 @@
+package ılerigithub;
+
+public class OmerYazici implements Quotation {
+    public int year() {
+        return 1950;
+    }
+    public String text() {
+        return "Ulu cɑmlɑr firtinɑli diyɑrlɑrdɑ yetisir.\n";
+    }
+    public String author() {
+        return "Cemil Meric";
+    }
+    public String reference() {
+        return "https://www.neguzelsozler.com/ozlu-sozler/cemil-meric-sozleri.html";
+    }
+}
