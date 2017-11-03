a696a33e4046c75418f2e7d3c41ee34e6c60b1d3
hatice kübra danış <haticekubra.danis@stu.fsm.edu.tr>
2017-11-03 11:47:42 +0300

    test

---
+++ quote/HaticeKubraDanis.java
@@ -0,0 +1,16 @@
+package quote;
+
+public class HaticeKubraDanis implements Quotation {
+    public int year() {
+        return 1980;
+    }
+    public String text() {
+        return "Korkarak yasiyorsan, yalnizca hayati seyredersin.";
+    }
+    public String author() {
+        return "Nietzsche";
+    }
+    public String reference() {
+        return "http://www.turkish-media.com/forum/topic/12176-nietzscheden-bir-mektup/";
+    }
+}
