3eb2fdcb062d8288d3b2adc15a25fb3472dd6d99
Unknown <aydogduenes.i@gmail.com>
2017-11-03 15:25:49 +0300

    IbrahimEnesAydogdu
    IbrahimEnesAydogdu

---
+++ quote/IbrahimEnesAydogdu.java
@@ -0,0 +1,16 @@
+package quote;
+
+public class IbrahimEnesAydogdu implements Quotation {
+    public int year() {
+        return 1453;
+    }
+    public String text() {
+        return "İmkânın sınırını görmek için imkansızı denemek lazım.";
+    }
+    public String author() {
+        return "Fatih Sultan Mehmet Han";
+    }
+    public String reference() {
+        return "http://www.sabah.com.tr/galeri/kultursanat/kitaplara-giren-unutulmaz-sozler/2";
+    }
+}
