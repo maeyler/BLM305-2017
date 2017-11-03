a1bf763dd6a1a14a0d851e059beed3adb32573cb
Unknown <sevginurbilgin@gmail.com>
2017-11-03 13:09:22 +0300

    test

---
+++ quote/sevgi.java
@@ -0,0 +1,17 @@
+package quote;
+
+public class sevgi implements Quotation {
+    public int year() {
+        return 1973;
+    }
+    public String text() {
+        return "Bugünün işini yarına bırakma :)) "
+              +"amount of well being with the minimum amount of consumption.";
+    }
+    public String author() {
+        return "Anonim";
+    }
+    public String reference() {
+        return "https://www.wikiwand.com/en/Small_Is_Beautiful";
+    }
+}
