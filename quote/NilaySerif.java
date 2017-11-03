27abb8ecec8be3721fde2fdcb2d129d30cabbae8
Unknown <nilayserif@gmail.com>
2017-11-03 22:07:33 +0200

    NilaySerif

---
+++ quote/NilaySerif.java
@@ -0,0 +1,16 @@
+package quote;
+
+public class NilaySerif implements Quotation {
+    public int year() {
+        return 0;
+    }
+    public String text() {
+        return "Her þeyin ölçüsü insandýr";
+    }
+    public String author() {
+        return "Pythagoras";
+    }
+    public String reference() {
+        return "http://www.meshursozler.com/meshur-sozleri/687-pisagor-sozleri.html";
+    }
+}
