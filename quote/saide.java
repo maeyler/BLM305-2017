7547855bcd09d8a4d2647f9d3064b6508cd3ac59
Unknown <s.baspehlivan@stu.fsm.edu.tr>
2017-11-03 12:53:17 +0300

    test

---
+++ quote/saide.java
@@ -0,0 +1,17 @@
+package quote;
+
+public class saide implements Quotation {
+    public int year() {
+        return 1973;
+    }
+    public String text() {
+        return "Gerçek değil düzme bir dünyaydı, okuduğum bütün okullarda, önüme konulan. Hayal gücümü harekete geçirmesem yıkılmıştım";
+            
+    }
+    public String author() {
+        return "Nuri Pakdil";
+    }
+    public String reference() {
+        return "https://www.wikiwand.com/en/Small_Is_Beautiful";
+    }
+}
