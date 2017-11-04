e8f9d4898864ef96616d4470ec74749a064e5493
Unknown <m.gulbahce@stu.fsm.edu.tr>
2017-11-04 09:16:56 +0300

    MuhammetAlıGulbahce

---
+++ quote/MAGulbahce.java
@@ -0,0 +1,16 @@
+package quote;
+
+public class MAGulbahce implements Quotation {
+    public int year() {
+        return 1530;
+    }
+    public String text() {
+        return  "Dünyada her ne var ise kaynaðý aþktýr; ilim ise koca bir dedikodu.";
+    }
+    public String author() {
+        return "Fuzuli";
+    }
+    public String reference() {
+        return "https://tr.wikiquote.org/wiki/Fuz%C3%BBl%C3%AE";
+    }
+}
