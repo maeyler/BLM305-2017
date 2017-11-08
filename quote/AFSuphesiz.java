cc903b20a8f1f7e8a59eeec3d1de6a75fb00a3e7
Unknown <ahmetsuphesiz@hotmail.com>
2017-11-08 11:21:59 +0300

    AhmetFırat

---
+++ quote/AFSuphesiz.java
@@ -0,0 +1,16 @@
+package quote;
+
+public class AFSuphesiz implements Quotation {
+    public int year() {
+        return 1938;
+    }
+    public String text() {
+        return "Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.";
+    }
+    public String author() {
+        return "Albert Einstein";
+    }
+    public String reference() {
+        return "https://www.goodreads.com/quotes/tag/science";
+    }
+}
