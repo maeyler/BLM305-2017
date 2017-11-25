eeb8f94ac0f9f6daacbb751e74c7e131eac74659
Unknown <furkan.sarihan@stu.fsm.edu.tr>
2017-11-25 13:10:53 +0300

    Some Changes
    Scanner removed from file

--- query/BetterGpa.java
+++ query/BetterGpa.java
@@ -1,27 +1,26 @@
 package query;
-import java.util.Scanner;
 
 public class BetterGpa implements Query {
-    public String defaultValue() { return "0,00";}
+    public String defaultValue() { return "0.00";}
     public String question() {
 		return "How many students they have better GPA than input number";
 	}
     public String doQuery(Database db, String input) {
-		
-		Scanner sc = new Scanner(input);
         double refGPA;
-        if(sc.hasNextDouble()){
-		refGPA = sc.nextDouble();
-		}else{
-			return "The program will be waiting for valid input\nNote that you must use comma!";}    
-        int count = 0;
-		for (Student s : db.std.values()){
-			if(s.gpa>refGPA){
-				count++;
-			}
-		}
-		System.out.println(count+" students have better GPA than "+input);
-		return String.valueOf(count);
+	if(input == null){ input = defaultValue();}
+	try{
+            refGPA = Double.parseDouble(input);
+	}catch(Exception e){
+            refGPA = Double.parseDouble(defaultValue());
 	}
+        int count = 0;
+        for (Student s : db.std.values()){
+            if(s.gpa>refGPA){
+                count++;
+            }	
+        }
+	System.out.println(count+" students have better GPA than "+input);
+	return String.valueOf(count);
+        }
     public String author() { return "Furkan Sarihan"; }
 }
